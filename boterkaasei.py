6e1cbc56b37760b303daaca40cb3a2b859796a7e
Hugo <45490945+hugodehaan@users.noreply.github.com>
2021-12-13 16:52:45 +0100

    gestart met validations

--- boterkaasei.py
+++ boterkaasei.py
@@ -1,4 +1,4 @@
-from bke import MLAgent, is_winner, opponent, train, save, load
+from bke import MLAgent, is_winner, opponent, train, save, load, validate, RandomAgent, plot_validation
  
  
 class MyAgent(MLAgent):
@@ -18,3 +18,9 @@ my_agent = load('MyAgent_3000')
 train(my_agent, 3000)
  
 save(my_agent, 'MyAgent_3000')
+
+validation_agent = RandomAgent()
+ 
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+ 
+plot_validation(validation_result)
